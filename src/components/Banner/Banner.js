f5fdd075128882a1316ba626f32f7d4c1c141721
zubayer <zubayer3570@gmail.com>
2022-04-17 17:28:58 +0600

    added banner

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { Carousel } from "react-bootstrap";
+import "./Banner.css";
+import banner1 from "../../images/banner/banner-1.png";
+import banner2 from "../../images/banner/banner-2.png";
+import banner3 from "../../images/banner/banner-3.png";
+
+const Banner = () => {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img className="d-block w-100" src={banner1} alt="First slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={banner2} alt="Second slide" />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={banner3} alt="Third slide" />
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+
+export default Banner;
